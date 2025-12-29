1e8219c3c109cc34be2124bf9d41f9b2eb5650da
vanesaromeo <vaneroeo09@gmail.com>
2025-12-28 23:57:05 -0300

    cambio modificado

--- index.ts
+++ index.ts
@@ -1,15 +1,16 @@
 import { Server } from "./models/server";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const server = new Server();
 
+
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 3002;
+  server.app.listen(PORT, () => {
+    console.log(`🚀 Backend corriendo en http://localhost:${PORT}`);
+  });
+}
+
 export default server.app;
-
-
-
-
-
-
-
